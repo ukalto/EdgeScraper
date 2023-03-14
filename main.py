810771c483070ce58eb42df1b0bd0afa9035200e
ukalto <maxi.gaber@gmail.com>
2023-03-14 15:34:59 +0100

    init

---
+++ main.py
@@ -0,0 +1,28 @@
+# pip install random-word
+# pip install selenium
+# find edge-profile with "edge://version/"
+# create copy of "User Data" profile if it doesn't work
+
+from random_word import RandomWords
+from selenium import webdriver
+
+r = RandomWords()
+
+if __name__ == '__main__':
+    edge_options = webdriver.EdgeOptions()
+    edge_options.use_chromium = True
+    edge_options.add_argument("--headless=new")
+    edge_options.add_argument("start-maximized")
+    edge_options.add_argument("user-data-dir=C:\\Users\\XYZ\\AppData\\Local\\Microsoft\\Edge\\User Data 1")
+    edge_options.add_argument("profile-directory=Profile")
+    edgeBrowser = webdriver.Edge(options=edge_options)
+    edgeBrowser.get("https://www.bing.com/")
+
+    for i in range(1, 11):
+        rw = r.get_random_word()
+        search_bar = edgeBrowser.find_element('xpath', '//*[@id="sb_form_q"]')
+        search_bar.clear()
+        search_bar.send_keys(rw)
+        search_bar.submit()
+        print(f"done {i}")
+    edgeBrowser.quit()
